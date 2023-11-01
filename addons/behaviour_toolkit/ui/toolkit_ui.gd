4034e52a1dbb17de6f428c651651e5a14d4a65c9
Pat <27511950+ThePat02@users.noreply.github.com>
2023-11-01 21:02:10 +0100

    add: version, tabs, buttons

--- addons/behaviour_toolkit/ui/toolkit_ui.gd
+++ addons/behaviour_toolkit/ui/toolkit_ui.gd
@@ -6,6 +6,23 @@ var current_selection: Node
 
 
 func _ready():
+	# Get current version of the plugin
+	var config = ConfigFile.new()
+
+	# Load data from a file.
+	var err = config.load("res://addons/behaviour_toolkit/plugin.cfg")
+	
+	# If the file didn't load, ignore it.
+	if err != OK:
+		return
+	
+	var plugin_version = config.get_value("plugin", "version")
+
+	%Version.text = "BehaviourToolkit v" + str(plugin_version)
+	
+	config.clear()
+
+	# Connect buttons
 	%ButtonState.connect("pressed", _on_button_pressed.bind(FSMState))
 	%ButtonTransition.connect("pressed", _on_button_pressed.bind(FSMTransition))
 
