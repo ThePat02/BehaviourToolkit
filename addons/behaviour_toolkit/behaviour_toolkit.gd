155ad3d191211cb1d3992b7ff9e869ceb489ea41
Pat <27511950+ThePat02@users.noreply.github.com>
2023-11-02 17:41:54 +0100

    misc: updated main icon

--- addons/behaviour_toolkit/behaviour_toolkit.gd
+++ addons/behaviour_toolkit/behaviour_toolkit.gd
@@ -1,3 +1,3 @@
-@icon("res://addons/behaviour_toolkit/icons/MainIcon.svg")
+@icon("res://addons/behaviour_toolkit/icons/Gear.svg")
 class_name BehaviourToolkit extends Node
 ## The main node of the BehaviourToolkit plugin.
