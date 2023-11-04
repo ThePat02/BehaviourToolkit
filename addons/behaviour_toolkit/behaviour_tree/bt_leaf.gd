b09e5dd6aabfbeb42d6991444ff2acaae181317f
Pat <27511950+ThePat02@users.noreply.github.com>
2023-11-04 20:59:21 +0100

    fix: add virtual method to BTLeaf

--- addons/behaviour_toolkit/behaviour_tree/bt_leaf.gd
+++ addons/behaviour_toolkit/behaviour_tree/bt_leaf.gd
@@ -1,2 +1,6 @@
 @icon("res://addons/behaviour_toolkit/icons/BTLeaf.svg")
 class_name BTLeaf extends BTBehaviour
+
+
+func tick(_actor: Node, _blackboard: Blackboard) -> Status:
+    return Status.SUCCESS
