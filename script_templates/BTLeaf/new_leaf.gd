4f9d5846de13fe6950a6ce5fc8d904cc3c0aa6b0
Pat <27511950+ThePat02@users.noreply.github.com>
2023-11-12 18:32:13 +0100

    fix: Optimized templates

--- script_templates/BTLeaf/new_leaf.gd
+++ script_templates/BTLeaf/new_leaf.gd
@@ -1,5 +1,8 @@
 extends BTLeaf
 
 
+# Gets called every tick of the behavior tree
 func tick(_actor: Node, _blackboard: Blackboard) -> Status:
+    # Handle leaf logic
+    # Return SUCCESS, FAILURE, or RUNNING
     return Status.SUCCESS
