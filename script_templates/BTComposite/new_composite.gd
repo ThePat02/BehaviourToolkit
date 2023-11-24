e49ecbd83ac40bb88055e954211eeb02f4288c30
Pat <27511950+ThePat02@users.noreply.github.com>
2023-11-24 12:04:28 +0100

    misc

--- script_templates/BTComposite/new_composite.gd
+++ script_templates/BTComposite/new_composite.gd
@@ -3,7 +3,7 @@ extends BTComposite
 
 # Gets called every tick of the behavior tree
 # `leaves` is an array of the children of this node set @onready
-func tick(actor: Node, blackboard: Blackboard) -> BTStatus:
+func tick(_actor: Node, _blackboard: Blackboard) -> BTStatus:
     # Logic for ticking one or more leaves
     # Return BTStatus depending on the result of the leaves
     # Return BTStatus.RUNNING, if there are still leaves to tick
