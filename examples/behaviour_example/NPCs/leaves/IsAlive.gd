98d417d1120d83f4f32f0b9abd768f6dd8653dae
Pat <27511950+ThePat02@users.noreply.github.com>
2023-11-30 10:34:07 +0100

    misc: Remove warnings from example

--- examples/behaviour_example/NPCs/leaves/IsAlive.gd
+++ examples/behaviour_example/NPCs/leaves/IsAlive.gd
@@ -1,7 +1,7 @@
 extends BTLeaf
 
 
-func tick(delta: float, actor: Node, _blackboard: Blackboard) -> BTStatus:
+func tick(_delta: float, actor: Node, _blackboard: Blackboard) -> BTStatus:
 	if actor.alive:
 		return BTStatus.SUCCESS
 	
