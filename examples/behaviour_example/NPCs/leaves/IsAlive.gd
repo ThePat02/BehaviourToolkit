f224723af8b9e2da0eda5b963d4c1722e4ce5187
Pat <27511950+ThePat02@users.noreply.github.com>
2024-01-04 14:24:23 +0100

    cleanup

--- examples/behaviour_example/NPCs/leaves/IsAlive.gd
+++
@@ -1,9 +0,0 @@
-extends BTLeaf
-
-
-func tick(_delta: float, actor: Node, _blackboard: Blackboard) -> BTStatus:
-	if actor.alive:
-		return BTStatus.SUCCESS
-	
-	return BTStatus.FAILURE
-
--- examples/behaviour_example/NPCs/leaves/IsHydrated.gd
+++
@@ -1,8 +0,0 @@
-extends BTLeaf
-
-
-func tick(_delta: float, actor: Node, _blackboard: Blackboard) -> BTStatus:
-	if actor.thirst == 0:
-		return BTStatus.FAILURE
-	
-	return BTStatus.SUCCESS
