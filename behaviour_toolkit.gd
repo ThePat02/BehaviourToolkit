d800f35cabfc0216406aa5cf6b96f105192dc09a
ThePat02 <27511950+ThePat02@users.noreply.github.com>
2024-01-04 18:01:34 +0000

    Updated Submodule from Main

--- behaviour_toolkit.gd
+++
@@ -1,5 +0,0 @@
-@icon("res://addons/behaviour_toolkit/icons/Gear.svg")
-class_name BehaviourToolkit extends Node
-## Base class for Behaviour Toolkit plugin nodes.
-##
-## Currently only used to group this plugins nodes.
